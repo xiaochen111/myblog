31abde42d9a5eb3192f4dae05720b4d81f3dc91a
chenhaibo <chb0413@163.com>
2020-01-08 14:26:35 +0800

    firstcommit

---
+++ service/app/controller/home.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const Controller = require('egg').Controller;
+
+class HomeController extends Controller {
+  async index() {
+    const { ctx } = this;
+    ctx.body = 'hi, egg';
+  }
+  async list() {
+    const { ctx } = this;
+    ctx.body = {
+      id: 1,
+    };
+  }
+}
+
+
+module.exports = HomeController;
