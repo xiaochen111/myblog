a7d8170474d7b229e3550220d851bb4da18cc817
chenhaibo <chb0413@163.com>
2020-01-09 10:17:09 +0800

    添加详情页

---
+++ blog/src/views/articleDetail/articleMd.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="list">
+        <vue-markdown :source="content" class="markdown-body"/>
+    </div>
+</template>
+
+
+<script>
+    import { getMd } from '@/common/api'
+    export default {
+        components:{
+            VueMarkdown:resolve => require(["vue-markdown"], resolve),
+        },
+        data() {
+            return {
+                content:''
+            }
+        },
+        created() {
+            this.getMdContent()
+        },
+        methods: {
+            async getMdContent(){
+                let res = await getMd();
+                this.content = res;
+            }
+        },
+    }
+</script>
+
+<style lang="less" scoped>
+    .list{
+        box-sizing: border-box; padding: 30px 20px;
+    }
+</style>
