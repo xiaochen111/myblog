b384f0124544b7cd5753c24fb5c59bdec2971e90
chenhaibo <chb0413@163.com>
2020-01-09 10:17:28 +0800

    添加详情接口

--- service/app/router.js
+++ service/app/router.js
@@ -7,4 +7,5 @@ module.exports = app => {
   const { router, controller } = app;
   router.get('/', controller.home.index);
   router.post('/blog/list', controller.home.list);
+  router.post('/blog/md', controller.home.getMdContent);
 };
