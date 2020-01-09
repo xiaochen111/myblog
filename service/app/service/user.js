566bc3c7c760298053bcf6598ffacd1b632033cf
chenhaibo <chb0413@163.com>
2020-01-09 13:09:54 +0800

    注释掉

--- service/app/service/user.js
+++ service/app/service/user.js
@@ -4,9 +4,9 @@ const Service = require('egg').Service;
 
 class UserService extends Service {
   async find() {
-    const user = await this.app.mysql.get('o_web_user', { id: '74078887616847872' });
-    console.log(user);
-    return user;
+    // const user = await this.app.mysql.get('o_web_user', { id: '74078887616847872' });
+    // console.log(user);
+    // return user;
   }
 }
 
