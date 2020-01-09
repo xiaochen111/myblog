ff2916d48195cafa07dce6974bb7510e93352215
chenhaibo <chb0413@163.com>
2020-01-09 10:18:56 +0800

    功能添加

--- blog/src/views/articleList/index.vue
+++ blog/src/views/articleList/index.vue
@@ -1,12 +1,35 @@
 <template>
     <div class="list">
-        文章列表
+        <ul>
+            <li v-for="(item,index) in list" :key="index">
+                {{ item.title }}{{index+1}}
+            </li>
+        </ul>
     </div>
 </template>
 
+
 <script>
+    import VueMarkdown from 'vue-markdown'
+    import { list } from '@/common/api';
     export default {
-        
+        components:{
+            VueMarkdown
+        },
+        data() {
+            return {
+                list:[]
+            }
+        },
+        created() {
+            this.getlist()
+        },
+        methods: {
+            async getlist(){
+                let res = await list();
+                this.list = res;
+            }
+        },
     }
 </script>
 
