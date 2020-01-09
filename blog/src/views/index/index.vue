ba061614ec5487baca2de022adcdd13531c9cf0f
chenhaibo <chb0413@163.com>
2020-01-09 10:17:55 +0800

    样式调整

--- blog/src/views/index/index.vue
+++ blog/src/views/index/index.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="main">
-        <div class="main-container">
+        <div class="main-container clearfix">
             <side-nav/>
             <div class="right-main">
                 <router-view />
@@ -24,12 +24,21 @@
 
 <style lang="less" scoped>
     #main{
-        height: 100%; background-color: #f5f7f9;
+        min-height: 100%; background-color: #f5f7f9;
         .main-container{
             width: 1200px; margin: 0 auto;
         }
         .right-main{
             width: 940px; background: #fff; float: right;
         }
+    }
+    .clearfix{zoom: 1;}
+    .clearfix:after {
+        visibility: hidden;
+        display: block;
+        font-size: 0;
+        content: "";
+        clear: both;
+        height: 0;
     }
 </style>
