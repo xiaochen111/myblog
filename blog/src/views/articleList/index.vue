bdf5a12c2b6e12542655275ffe93e740c43f730a
chenhaibo <chb0413@163.com>
2020-01-09 15:29:45 +0800

    添加小动画

--- blog/src/views/articleList/index.vue
+++ blog/src/views/articleList/index.vue
@@ -45,13 +45,24 @@
     }
     ul{
         li{
-            line-height: 40px; border-bottom:1px solid @border;
+            border-bottom:1px solid @border;
+            animation: showLi .5s ease-out both;
             &:hover{
                 cursor: pointer;
             }
             a{
                 text-decoration: none; color: @darkColor;
             }
+        }
+    }
+    @keyframes showLi {
+        0% {
+            opacity: 0;
+            line-height: 30px;
+        }
+        100% {
+            opacity: 1;
+            line-height: 40px;
         }
     }
 </style>
