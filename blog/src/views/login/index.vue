8806516fe4614633b8d1d6a9d73dbcb0fdefac53
chenhaibo <chb0413@163.com>
2020-01-13 14:35:55 +0800

    页面编写

--- blog/src/views/login/index.vue
+++ blog/src/views/login/index.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        login
+    <div class="login">
+        <p class="tit">登录</p>
+        <div class="title">
+            <p class="label">用户名</p>
+            <input type="text">
+        </div>
+        <div class="title">
+            <p class="label">密码</p>
+            <input type="password">
+        </div>
+        <div class="btn">
+            <button>登录</button>
+        </div>
     </div>
 </template>
 
@@ -11,5 +22,29 @@
 </script>
 
 <style lang="less" scoped>
-
+    .login{
+        width: 450px; .juzhong(); border: 1px solid @border; border-radius: 3px;
+        box-shadow: -12px 0 20px -12px rgba(0, 0, 0, 0.35), 12px 0 20px -12px rgba(0, 0, 0, 0.35);
+        box-sizing: border-box; padding: 30px 40px;
+    }
+    .tit{
+        line-height: 1.8; font-size: 22px; text-align: center; margin-bottom: 20px;
+    }
+    .title{
+      padding-bottom: 20px;
+      .label{
+        line-height: 1.8; margin-bottom: 10px;
+      }
+      input{
+        display: block; line-height:35px; border:1px solid @border; outline: none;
+        width: 100%;  text-indent: 10px; border-radius: 3px;
+      }
+    }
+    .btn{
+        text-align: center;
+        button{
+            padding: 10px 45px; background: @btnBlue; color: #fff; outline: none; border: none; margin: 20px 0;
+            border-radius: 3px; cursor: pointer;
+        }
+    }
 </style>
